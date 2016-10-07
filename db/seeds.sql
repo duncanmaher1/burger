43ab4fb56fca89024b74a8106d38b974ed8ec33b
Duncan Maher <duncan.maher1@Duncans-MacBook-Pro.local>
2016-10-06 17:05:42 -0700

    test

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers(burger_name, devoured) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Blue Cheese', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Double Bacon Cheese Burger', true);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Cheese Burger', false);
+
+
+
+
+
+
